749521515b946f8914f79c15ddd8fa4b0420efc1
Rishi Krishna Thodupunuri <rishi.thodupunuri@gmail.com>
2023-04-25 11:32:42 -0400

    added cors

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const server = http.createServer((req, res) => {
             (err, content) => {
 
                 if (err) throw err;
+                res.setHeader("Access-Control-Allow-Origin", "*");
                 res.writeHead(200, { 'Content-type': 'text/html' });
                 res.end(content);
 
@@ -25,6 +26,8 @@ const server = http.createServer((req, res) => {
     else if (req.url.startsWith('/Images/')) {
         const imagePath = path.join(__dirname, 'public', req.url);
         const imageStream = fs.createReadStream(imagePath);
+
+        res.setHeader("Access-Control-Allow-Origin", "*");
 
         
         if (req.url.match(/.*\.jpg$/i)) {
@@ -47,6 +50,7 @@ const server = http.createServer((req, res) => {
     else if (req.url === '/style.css') { // add this else if statement to serve style.css
         fs.readFile(path.join(__dirname, 'public', 'style.css'), (err, content) => {
             if (err) throw err;
+            res.setHeader("Access-Control-Allow-Origin", "*");
             res.writeHead(200, { 'Content-type': 'text/css' });
             res.end(content);
         });
@@ -55,6 +59,7 @@ const server = http.createServer((req, res) => {
     else if (req.url === '/script.js') { // add this else if statement to serve script.css
         fs.readFile(path.join(__dirname, 'public', 'script.js'), (err, content) => {
             if (err) throw err;
+            res.setHeader("Access-Control-Allow-Origin", "*");
             res.writeHead(200, { 'Content-type': 'application/javascript' });
             res.end(content);
         });
@@ -114,6 +119,7 @@ const server = http.createServer((req, res) => {
         }
 
         function processData(data) {
+            res.setHeader("Access-Control-Allow-Origin", "*");
                 res.writeHead(200, { 'Content-type': 'application/json' })
                 res.end(JSON.stringify(data));
         }
@@ -122,6 +128,7 @@ const server = http.createServer((req, res) => {
 
     }
     else {
+        res.setHeader("Access-Control-Allow-Origin", "*");
         res.writeHead(404, { 'Content-type': 'text/html' })
         res.end("<h1> 404 Nothing is Here </h1>")
     }
